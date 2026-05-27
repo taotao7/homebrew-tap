b2b3299f645629cf179c3190ae29c2cd69f2c0c7
taotao7 <moca_tao7@foxmail.com>
2026-05-27 16:50:25 +0800

    feat: publish theme-tape 0.1.21

--- Formula/theme-tape.rb
+++ Formula/theme-tape.rb
@@ -1,9 +1,9 @@
 class ThemeTape < Formula
   desc "Terminal theme switcher for Cassette Futurism and Zenith"
   homepage "https://github.com/taotao7/theme-tape"
-  version "0.1.20"
-  url "https://github.com/taotao7/theme-tape/releases/download/theme-tape-v0.1.20/theme-tape-0.1.20-darwin-arm64.tar.gz"
-  sha256 "180a2ef06a16a4f745694536e16f5ea9edf6441a40814dfb1ca8276651b3aa52"
+  version "0.1.21"
+  url "https://github.com/taotao7/theme-tape/releases/download/theme-tape-v0.1.21/theme-tape-0.1.21-darwin-arm64.tar.gz"
+  sha256 "7010de1e0ff4c93bf7954506afd327c5f25bc1e6e7f39ec6d2782b21d16bd560"
   license "MIT"
 
   depends_on :macos
