14e7b9622c1461ae8cefe9593ddb5b81405e6878
taotao7 <moca_tao7@foxmail.com>
2026-06-12 15:17:46 +0800

    Fix SpeakPatch cask audit

--- Casks/speakpatch.rb
+++ Casks/speakpatch.rb
@@ -2,8 +2,7 @@ cask "speakpatch" do
   version "0.1.0"
   sha256 "dbd98ebd8b1b06e831b756a528016180e477175f7051cdbd398f3256b983a25b"
 
-  url "https://github.com/taotao7/SpeakPatch/releases/download/v#{version}/SpeakPatch-#{version}-macos.zip",
-      verified: "github.com/taotao7/SpeakPatch/"
+  url "https://github.com/taotao7/SpeakPatch/releases/download/v#{version}/SpeakPatch-#{version}-macos.zip"
   name "SpeakPatch"
   desc "Menu-bar assistant for rewriting selected text"
   homepage "https://github.com/taotao7/SpeakPatch"
