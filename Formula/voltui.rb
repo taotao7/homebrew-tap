fcb5ee1458dab2b13858e047060177f894ef7af3
taotao7 <moca_tao7@foxmail.com>
2026-09-11 18:10:23 +0800

    feat: publish voltui 0.1.0

---
+++ Formula/voltui.rb
@@ -0,0 +1,37 @@
+class Voltui < Formula
+  desc "Cassette-futurism TUI for macOS connected-device batteries"
+  homepage "https://github.com/taotao7/voltui"
+  url "https://github.com/taotao7/voltui/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "af8db071651cccc8559e3659130b64e44bf771abf6bb0e944a09f4ae4ecff540"
+  license "MIT"
+  head "https://github.com/taotao7/voltui.git", branch: "main"
+
+  depends_on :macos
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/voltui --version")
+    assert_match "Mac", shell_output("#{bin}/voltui --demo --once")
+  end
+
+  def caveats
+    <<~EOS
+      voltui reads macOS accessory batteries via pmset / IOKit.
+
+        voltui                  # live widget
+        voltui --theme beige-terminal
+        voltui --init-config    # ~/.config/voltui/config.toml
+
+      Palettes: magnetic-night, beige-terminal
+      https://github.com/taotao7/cassette-futurism-theme
+    EOS
+  end
+end
