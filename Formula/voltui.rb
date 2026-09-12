c7c3d7a2a652a4ee04bb3306f46c5d0b9dc113ba
taotao7 <moca_tao7@foxmail.com>
2026-09-12 18:17:44 +0800

    feat: publish voltui 0.1.6

--- Formula/voltui.rb
+++ Formula/voltui.rb
@@ -1,13 +1,13 @@
 class Voltui < Formula
   desc "Cassette-futurism TUI for macOS connected-device batteries"
   homepage "https://github.com/taotao7/voltui"
-  url "https://github.com/taotao7/voltui/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "cf6ebb448f8878b2218fc311b2651d1864f257d3b984eae16cf34d21632275ae"
+  url "https://github.com/taotao7/voltui/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "a51cfe12356d99bc8a990f894f9ba5da25cb0eb83e462f0fa7e16c06b0689232"
   license "MIT"
   head "https://github.com/taotao7/voltui.git", branch: "main"
 
-  depends_on "go" => :build
   depends_on :macos
+  depends_on "go" => :build
 
   def install
     ldflags = %W[
@@ -15,6 +15,11 @@ class Voltui < Formula
       -X main.version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/voltui --version")
+    assert_match "Mac", shell_output("#{bin}/voltui --demo --once")
   end
 
   def caveats
@@ -28,10 +33,5 @@ class Voltui < Formula
       Palettes: magnetic-night, beige-terminal
       https://github.com/taotao7/cassette-futurism-theme
     EOS
-  end
-
-  test do
-    assert_match version.to_s, shell_output("#{bin}/voltui --version")
-    assert_match "Mac", shell_output("#{bin}/voltui --demo --once")
   end
 end
